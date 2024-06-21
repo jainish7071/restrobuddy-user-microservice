1ebee1a37f2d6f495d53d2165ce8a8402b45d3c3
Jainish Kunjadiya <jainishkunjadiya2712@gmail.com>
2024-06-21 21:38:12 +0530

    initial commit

---
+++ src/main/java/com/app/restrobuddy/restrobuddyuser/RestrobuddyuserApplication.java
@@ -0,0 +1,13 @@
+package com.app.restrobuddy.restrobuddyuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestrobuddyuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestrobuddyuserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/restrobuddy/restrobuddyuser/RestrobuddyuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.restrobuddy.restrobuddyuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestrobuddyuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
